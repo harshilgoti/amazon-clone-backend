511985e4d4f061aef921d6ffada116fd78827cdd
Harshil Goti <hgoti@plusonex.com>
2025-01-28 15:00:54 +0530

    Server file added

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import cors from 'cors';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
@@ -11,15 +11,16 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('Connected to MongoDB'))
-.catch((err) => console.error('MongoDB connection error:', err));
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
-app.get('/', (req, res) => {
-  res.send('Amazon Clone API is running');
+app.get("/", (req, res) => {
+  res.send("Amazon Clone API is running");
 });
 
 app.listen(PORT, () => {
